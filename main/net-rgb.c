af350d452e40fc0360505fbfd418a60623fede30
amouday <amouday@proton.me>
2026-08-29 17:08:59 -0700

    Added hexdump() and icmp packet handling routine.

--- main/net-rgb.c
+++ main/net-rgb.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <stdarg.h>
 
 #include "lwip/ip4_addr.h"
+#include "lwip/sockets.h"
 #include "esp_event.h"
+#include "esp_log.h"
 #include "nvs_flash.h"
 #include "ap_config.h"
 
+#define BUFSIZE				512
+#define HEXDUMP_LINELEN		16
+#define SRC_ADDR_OFFSET		12
 
 esp_netif_t *ap_handle = 0;
 
@@ -23,13 +29,56 @@ void setup_ap();
 void configure_wifi();
 void configure_ip();
 void err_halt(char*,...);
+void hexdump(uint8_t *, size_t);
+void hexdump_line(uint8_t *, size_t);
+void handle_packet(uint8_t *, size_t);
+int extract_src_addr(char *, uint8_t *);
 
-void app_main(void)
+void 
+app_main(void)
 {
+	int			sockfd;
+	uint8_t		buf[BUFSIZE];
+	ssize_t		recvlen;
+
 	setup_ap();
 
+	if ((sockfd = lwip_socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) == -1)
+		err_halt("lwip_socket failed");
+
 	while (1) {
+		if ((recvlen = lwip_recv(sockfd, buf, BUFSIZE, 0)) == -1) {
+			ESP_LOGE("recv", "recv failed");
+			continue;
+		}
+
+		handle_packet(buf, recvlen);
 	}
+}
+
+void 
+handle_packet(uint8_t *packet, size_t len)
+{
+	char		src_addr[INET_ADDRSTRLEN + 1];
+
+	extract_src_addr(src_addr, packet);
+	src_addr[INET_ADDRSTRLEN] = '\0';
+	printf("Recived ICMP Message from %s\n", src_addr);
+
+	hexdump(packet, len);
+
+	puts("");
+}
+
+int
+extract_src_addr(char *addrbuf, uint8_t *packet)
+{
+	packet += SRC_ADDR_OFFSET;
+
+	if (inet_ntop(AF_INET, packet, addrbuf, INET_ADDRSTRLEN) == NULL)
+		return -1;
+
+	return 0;
 }
 
 void 
@@ -80,6 +129,53 @@ configure_ip()
 void 
 err_halt(char *fmt,...)
 {
-	while (1)
-		;
+	va_list		ap;
+
+	va_start(ap, fmt);
+
+	while (1) {
+		vfprintf(stderr, fmt, ap);
+		fprintf(stderr, "\n");
+	}
+}
+
+void 
+hexdump(uint8_t *buf, size_t len)
+{
+	size_t		linelen;
+
+	if (buf == NULL)
+		return;
+
+	for (size_t i = 0; i < len; i += 16) {
+		/* if remaining bytes is less than 8, use that instead */
+		linelen = ((len - i) < 16) ? (len - i) : 16;		
+		hexdump_line(buf, linelen);
+		buf += linelen;
+	}
+}
+
+void 
+hexdump_line(uint8_t *line, size_t linelen)
+{
+	uint8_t		padding = 0;
+
+	padding = HEXDUMP_LINELEN - linelen;
+
+	for (int i = 0; i < linelen; i++)
+		printf("%.2x ", line[i]);
+
+	for (int i = 0; i < padding; i++)
+		printf("   ");
+	putchar('\t');
+
+	putchar('|');
+	for (int i = 0; i < linelen; i++) {
+		if (isprint(line[i]))
+			putchar(line[i]);
+		else
+			putchar('.');
+	}
+
+	printf("|\n");
 }
